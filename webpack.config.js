b29d8c77f30c9fc49bca8cf4d40e966e50a9d872
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2020-01-12 02:35:53 +0100

    chore: Basic setup, aaaallll the dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-env node */
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "static", "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
